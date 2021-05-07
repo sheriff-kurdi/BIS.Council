cd29af3b401791cdc6c1c31bd0d2d3125975bbad
sheriff.kurdi <sheriff.kurdi@gmail.com>
2021-05-07 00:48:41 -0700

    Add project files.

---
+++ BIS.Council.Domain/MaglesData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIS.Council.Domain
+{
+    public class MaglesData
+    {
+        public int Id { get; set; }
+        public int MaglesNo { get; set; }
+
+    }
+}
